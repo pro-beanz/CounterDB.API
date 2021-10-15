96e90d051ad6cc524a3556b172857b63c738f9b2
beanz <vegamehra@gmail.com>
2021-10-15 17:16:45 -0400

    Add project files.

---
+++ CounterDB.API/Models/CounterData.cs
@@ -0,0 +1,11 @@
+﻿namespace CounterDB.API.Models
+{
+    public class CounterData
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public int Count { get; set; }
+    }
+}
